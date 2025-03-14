acf6b8a1a1ab58a4218830aa66576647fe7bf1fa
Abhay2133 <63441094+Abhay2133@users.noreply.github.com>
2025-03-14 16:46:00 +0000

    Deploying to gh-pages from @ Abhay2133/acrilc@5878b173538ea9a1b72ab70350f23a3762ad60dd 🚀

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"18b71d647a292a980abb405ac7d16fe1f0b204
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "244162637"
+    serviceWorkerVersion: "418825064"
   }
 });
