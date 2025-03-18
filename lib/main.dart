9a3ad73a53bce2c1d89c24ef7ef6409d2fffd12a
abhay2133 <abhaybishthestudent@gmail.com>
2025-03-19 00:48:57 +0530

    fixed .env issue

--- lib/main.dart
+++ lib/main.dart
@@ -4,7 +4,7 @@ import 'router/app_router.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 Future<void> main() async {
-  await dotenv.load();
+  await dotenv.load(fileName: ".env");
   runApp(MyApp());
 }
 
