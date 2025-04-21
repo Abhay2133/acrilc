64d5e989b6197a4501d87b5e1e1168c82b4f6f8c
Abhay2133 <63441094+Abhay2133@users.noreply.github.com>
2025-04-21 18:13:53 +0000

    Deploying to gh-pages from @ Abhay2133/acrilc@deed59c943c639f1b9047972a334a1ca1042b717 🚀

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -57,7 +57,7 @@ const RESOURCES = {"canvaskit/skwasm.js": "f2ad9363618c5f62e813740099a80e63",
 "icons/Icon-maskable-192.png": "c445cead57efb8cb22e09c126ae784f4",
 "manifest.json": "d835103d40c17ff66f6b22fbc0196bc9",
 "favicon.png": "b4f7c72cc8e57325203af704b0f0dadd",
-"flutter_bootstrap.js": "546dfcfdb929755c9135bcb69fb91e0d"};
+"flutter_bootstrap.js": "cfd8d33c333f4f40518fc75c58eda31f"};
 // The application shell files that are downloaded before a service worker can
 // start.
 const CORE = ["main.dart.js",
